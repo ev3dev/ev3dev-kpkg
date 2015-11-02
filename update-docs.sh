44c24be02e7849b8a441b5a15fd356fc35e87358
David Lechner <david@lechnology.com>
2015-11-01 23:53:17 -0600

    Add parser for motor docs

--- update-docs.sh
+++ update-docs.sh
@@ -13,6 +13,7 @@ rm -f ${WEBSITE}/docs/drivers/*.markdown
 	drivers/lego/ev3/legoev3_ports_core.c \
 	drivers/lego/motors/{dc,servo,tacho}_motor_class.c \
 	drivers/lego/motors/rcx_{led,motor}.c \
+	drivers/lego/pistorms/pistorms_core.c \
 	drivers/lego/sensors/{ev3,nxt}_analog_sensor_core.c \
 	drivers/lego/sensors/ev3_uart_sensor_ld.c \
 	drivers/lego/sensors/ht_nxt_smux_i2c_sensor.c \
@@ -32,9 +33,19 @@ rm -f ${WEBSITE}/docs/sensors/*.markdown
 		drivers/lego/sensors/*_defs.c \
 		drivers/lego/wedo/wedo_{hub,sensor}.c
 
+rm -f ${WEBSITE}/docs/motors/*.markdown
+./motor-defs-to-markdown.py \
+	--kernel ${KERNEL} \
+	--website ${WEBSITE} \
+	--header-files \
+		drivers/lego/motors/ev3_motor.h \
+	--source-files \
+		drivers/lego/motors/*_defs.c
+
 rm -f ${WEBSITE}/docs/ports/*.markdown
 ./port-defs-to-markdown.py ${KERNEL} ${WEBSITE} \
 	drivers/lego/brickpi/brickpi_ports_{in,out}.c \
 	drivers/lego/ev3/legoev3_ports_{in,out}.c \
-	drivers/lego/sensors/{ht_nxt_smux,ms_ev3_smux}.c \
+	drivers/lego/pistorms/pistorms_ports_in.c \
+	drivers/lego/sensors/{ht_nxt_smux,ms_ev3_smux,ms_nxtmmx}.c \
 	drivers/lego/wedo/wedo_port.c
